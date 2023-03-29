88ea3904366b1fd2d804213b5f0981e8beaac6fc
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-03-30 00:03:11 +0300

    update

--- Assets/Scripts/RTSScripts/RTSFormation/FormationManager.cs
+++ Assets/Scripts/RTSScripts/RTSFormation/FormationManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FormationManager : Singleton<FormationManager>
 {
-    #region params
+    #region Params
     public FormationTypes CurrentFormationType;
     private FormationTypes cachedFormationType;
     public int _unitXOffset = 5;
@@ -12,151 +12,43 @@ public class FormationManager : Singleton<FormationManager>
     #region FormationStates
     FormationBase currentFormation;
     public BoxFormation boxFormation = new BoxFormation();
-    public LineFormation lineFormation=new LineFormation();
-    public CircleFormation circleFormation=new CircleFormation();
-    public TriangleFormation triangleFormation=new TriangleFormation();
+    public LineFormation lineFormation = new LineFormation();
+    public CircleFormation circleFormation = new CircleFormation();
+    public TriangleFormation triangleFormation = new TriangleFormation();
     #endregion
     #endregion
+    #region Methods
     private void Start()
     {
-        cachedFormationType = CurrentFormationType;
         SetFormationType();
+        cachedFormationType = CurrentFormationType;
     }
     private void SetFormationType()
-    {
-        if(cachedFormationType== CurrentFormationType)
-        {
-            currentFormation.CalculateFormation();
-        }
-        else
-        {
-            switch (CurrentFormationType)
-            {
-                case FormationTypes.Box:
-                    currentFormation = boxFormation;
-                    break;
-                case FormationTypes.Circle:
-                    currentFormation = circleFormation;
-                    break;
-                case FormationTypes.Line:
-                    currentFormation = lineFormation;
-                    break;
-                case FormationTypes.Triangle:
-                    currentFormation = triangleFormation;
-                    break;
-            }
-        }
-
-    }
-    #region MyMethods
-    /*public void CalculateFormationPos(Vector3 mousePoint)
     {
         switch (CurrentFormationType)
         {
             case FormationTypes.Box:
-                //OrganizeCharactersInSquare(mousePoint);
-                currentFormation.CalculateFormation(mousePoint);
+                currentFormation = boxFormation;
                 break;
             case FormationTypes.Circle:
-                OrganizeCharactersInCircle(mousePoint);
+                currentFormation = circleFormation;
                 break;
             case FormationTypes.Line:
-                OrganizeCharactersInRows(mousePoint);
+                currentFormation = lineFormation;
                 break;
             case FormationTypes.Triangle:
-                ArrangeCharactersInTriangleFormation(mousePoint);
-                break;
-            default:
+                currentFormation = triangleFormation;
                 break;
         }
     }
-    #region CalculateFormations
-    public void ArrangeCharactersInTriangleFormation(Vector3 mousePoint)
+    
+    public void CalculateFormationPos(Vector3 mousePoint)
     {
-        int totalCalculated = 0;
-        int rowCount = CalculateM(RTSManager.Instance.SelectedCharacters.Count);
-        float halfDistance = _unitXOffset / 2f;
 
-        RTSManager.Instance.SelectedCharacters.Sort((x, y) => x.transform.position.x.CompareTo(y.transform.position.x));
-
-        int index = 0;
-        for (int row = 0; row < rowCount; row++)
-        {
-            int rowSize = rowCount - row;
-
-            for (int i = 0; i < rowSize; i++)
-            {
-                totalCalculated++;
-                if (totalCalculated > RTSManager.Instance.SelectedCharacters.Count)
-                    return;
-
-                float xPos = mousePoint.x + (i - (rowSize - 1) / 2f) * _unitXOffset;
-                float zPos = mousePoint.z + (row * _unitZOffset) - halfDistance;
-                RTSManager.Instance.SelectedCharacters[index].GetComponent<RTSControl>().targetPos = new Vector3(xPos, mousePoint.y, zPos);
-                index++;
-            }
-        }
-    }
-    public void OrganizeCharactersInRows(Vector3 mousePoint)
-    {
-        int numRows = Mathf.CeilToInt((float)RTSManager.Instance.SelectedCharacters.Count / 6); //row sayýsýný belirler
-        int numPerRow = Mathf.CeilToInt((float)RTSManager.Instance.SelectedCharacters.Count / numRows); //row baþýna karakter sayýsýný belirler
-
-        for (int i = 0; i < RTSManager.Instance.SelectedCharacters.Count; i++)
-        {
-            int row = i / numPerRow;
-            int col = i % numPerRow;
-            Vector3 newPos = mousePoint + new Vector3(col * _unitXOffset, 0, row * _unitZOffset);
-            RTSManager.Instance.SelectedCharacters[i].GetComponent<RTSControl>().targetPos = newPos;
-        }
-    }
-    public void OrganizeCharactersInSquare(Vector3 mousePoint)
-    {
-        int numRows = Mathf.CeilToInt(Mathf.Sqrt(RTSManager.Instance.SelectedCharacters.Count)); //kare boyutunu belirler
-
-        for (int i = 0; i < RTSManager.Instance.SelectedCharacters.Count; i++)
-        {
-            int row = i / numRows;
-            int col = i % numRows;
-            Vector3 newPos = mousePoint + new Vector3(col * _unitXOffset, 0, row * _unitZOffset);
-            newPos.y = 0;
-            RTSManager.Instance.SelectedCharacters[i].GetComponent<RTSControl>().targetPos = newPos;
-        }
-    }
-    public void OrganizeCharactersInCircle(Vector3 mousePoint)
-    {
-        int numCharacters = RTSManager.Instance.SelectedCharacters.Count;
-        float angleBetweenCharacters = 360f / numCharacters;
-
-        for (int i = 0; i < numCharacters; i++)
-        {
-            float angle = i * angleBetweenCharacters * Mathf.Deg2Rad;
-            float x = Mathf.Cos(angle) * _unitXOffset;
-            float y = Mathf.Sin(angle) * _unitZOffset;
-            Vector3 newPos = mousePoint + new Vector3(x, 0, y);
-            RTSManager.Instance.SelectedCharacters[i].GetComponent<RTSControl>().targetPos = newPos;
-        }
-    }
-    public static int CalculateM(int total)
-    {
-        int m = 1;
-        while (m * (m + 1) / 2 < total)
-        {
-            m++;
-        }
-        return m;
-    }
-    #endregion
-    */
-    #endregion
-
-    private void Update()
-    {
-        if(cachedFormationType!=CurrentFormationType)
-        {
+        if (cachedFormationType != CurrentFormationType)
             SetFormationType();
-            cachedFormationType = CurrentFormationType;
-        }
+        currentFormation.CalculateFormation(mousePoint);
     }
+    #endregion
 }
 
